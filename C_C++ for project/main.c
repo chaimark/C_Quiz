ae83a0803d492cce3018b67a0090e9fb8945d094
chaimark <chai21sks@gmail.com>
2024-07-14 23:00:44 +0800

    add a

--- C_C++ for project/main.c
+++ C_C++ for project/main.c
@@ -1,8 +1,12 @@
 #include "./MyCmdDos/CsvSplit.h"
 #include "./array_quiz/ArrayQuiz.h"
+#include "./C_MyLib/NumberBaseLib.h"
+#include "./C_MyLib/StrLib.h"
+#include "./C_MyLib/WinExcel.h"
 
 int main(int argc, char *argv[]) {
-	csvSplit(argc, argv);
-    //arrayQuiz();
+	// csvSplit(argc, argv);
+    // arrayQuiz();
+    //
 }
 
