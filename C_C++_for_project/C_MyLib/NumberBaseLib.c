1c6e24d16a9fcc14c01e39494d9c3da869426c24
chai21sks <3046476712@qq.com>
2025-02-16 16:20:09 +0800

    更新

--- C_C++_for_project/C_MyLib/NumberBaseLib.c
+++ C_C++_for_project/C_MyLib/NumberBaseLib.c
@@ -3,12 +3,47 @@
 #include <stdio.h>
 
 void DoubleOrFloatToBuff(strnew OutBuff, double Number, bool IsDouble) {
-    if ((IsDouble) && (OutBuff.MaxLen >= sizeof(double))) {
-        memcpy(OutBuff.Name._char, &Number, sizeof(double));
-    } else if(OutBuff.MaxLen >= sizeof(float)) {
-        float tempNum = (float)Number;
-        memcpy(OutBuff.Name._char, &tempNum, sizeof(float));
+#if __STDC_VERSION__ >= 199901L
+    /*Now C99*/
+    int ArraySize = 0;
+    if (IsDouble) {
+        ArraySize = sizeof(double);
+    } else {
+        ArraySize = sizeof(float);
     }
+    char TempBuff[ArraySize];
+    memset(TempBuff, 0, ArraySize);
+    if ((IsDouble) && (OutBuff.MaxLen >= ArraySize)) {
+        memcpy(TempBuff, &Number, ArraySize);
+        swapStr(TempBuff, ArraySize);
+        memcpy(OutBuff.Name._char, TempBuff, ArraySize);
+    } else if (OutBuff.MaxLen >= ArraySize) {
+        float tempNum = (float)Number;
+        memcpy(TempBuff, &tempNum, ArraySize);
+        swapStr(TempBuff, ArraySize);
+        memcpy(OutBuff.Name._char, TempBuff, ArraySize);
+    }
+#else
+    /*Not C99*/
+    int ArraySize = 0;
+    if (IsDouble) {
+        ArraySize = sizeof(double);
+    } else {
+        ArraySize = sizeof(float);
+    }
+    char * TempBuff = (char *)malloc(ArraySize);
+    memset(TempBuff, 0, ArraySize);
+    if ((IsDouble) && (OutBuff.MaxLen >= ArraySize)) {
+        memcpy(TempBuff, &Number, ArraySize);
+        swapStr(TempBuff, ArraySize);
+        memcpy(OutBuff.Name._char, TempBuff, ArraySize);
+    } else if (OutBuff.MaxLen >= ArraySize) {
+        float tempNum = (float)Number;
+        memcpy(TempBuff, &tempNum, ArraySize);
+        swapStr(TempBuff, ArraySize);
+        memcpy(OutBuff.Name._char, TempBuff, ArraySize);
+    }
+#endif
 }
 /*
 // 该库中提到的所有进制数全是直接表示, 比如
