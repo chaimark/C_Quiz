809ae755152cd2abcdb1720a25d42706b281f84f
chai21sks <chai21sks@gmail.com>
2025-02-22 10:59:46 +0800

    change demo

--- C_C++_for_project/C_MyLib/NumberBaseLib.c
+++ C_C++_for_project/C_MyLib/NumberBaseLib.c
@@ -3,48 +3,31 @@
 #include <stdio.h>
 
 void DoubleOrFloatToBuff(strnew OutBuff, double Number, bool IsDouble) {
-#if __STDC_VERSION__ >= 199901L
-    /*Now C99*/
-    int ArraySize = 0;
+    uint8_t BuffLen = 0;
+    union {
+        double TempDouble;
+        float TempFloat;
+        uint64_t Buff8;
+    } converter;
     if (IsDouble) {
-        ArraySize = sizeof(double);
+        if (OutBuff.MaxLen < 8) {
+            return;
+        }
+        converter.TempDouble = Number;
+        BuffLen = 8;
     } else {
-        ArraySize = sizeof(float);
+        if (OutBuff.MaxLen < 4) {
+            return;
+        }
+        converter.TempFloat = Number;
+        BuffLen = 4;
     }
-    char TempBuff[ArraySize];
-    memset(TempBuff, 0, ArraySize);
-    if ((IsDouble) && (OutBuff.MaxLen >= ArraySize)) {
-        memcpy(TempBuff, &Number, ArraySize);
-        swapStr(TempBuff, ArraySize);
-        memcpy(OutBuff.Name._char, TempBuff, ArraySize);
-    } else if (OutBuff.MaxLen >= ArraySize) {
-        float tempNum = (float)Number;
-        memcpy(TempBuff, &tempNum, ArraySize);
-        swapStr(TempBuff, ArraySize);
-        memcpy(OutBuff.Name._char, TempBuff, ArraySize);
+    for (int i = 0; i < BuffLen; i++) {
+        OutBuff.Name._char[i] = (uint8_t)(converter.Buff8 >> (i * 8) & 0xFF);
     }
-#else
-    /*Not C99*/
-    int ArraySize = 0;
-    if (IsDouble) {
-        ArraySize = sizeof(double);
-    } else {
-        ArraySize = sizeof(float);
-    }
-    char * TempBuff = (char *)malloc(ArraySize);
-    memset(TempBuff, 0, ArraySize);
-    if ((IsDouble) && (OutBuff.MaxLen >= ArraySize)) {
-        memcpy(TempBuff, &Number, ArraySize);
-        swapStr(TempBuff, ArraySize);
-        memcpy(OutBuff.Name._char, TempBuff, ArraySize);
-    } else if (OutBuff.MaxLen >= ArraySize) {
-        float tempNum = (float)Number;
-        memcpy(TempBuff, &tempNum, ArraySize);
-        swapStr(TempBuff, ArraySize);
-        memcpy(OutBuff.Name._char, TempBuff, ArraySize);
-    }
-#endif
+    swapStr(OutBuff.Name._char, BuffLen);
 }
+
 /*
 // 该库中提到的所有进制数全是直接表示, 比如
 // hex(0x125)   //表示 dec(125)
--- C_C++_for_project/main.c
+++ C_C++_for_project/main.c
@@ -13,83 +13,67 @@
 // #include "./mbedtls/token.h"
 #include <stdio.h>
 
-// void DoubleOrFloatToBuff(strnew OutBuff, double Number, bool IsDouble) {
-// #if __STDC_VERSION__ >= 199901L
-//     /*Now C99*/
-//     int ArraySize = 0;
-//     if (IsDouble) {
-//         ArraySize = sizeof(double);
-//     } else {
-//         ArraySize = sizeof(float);
+// // IPstrToHexArray 函数定义
+// void IPstrToHexArray(strnew IpHex, const char * Ipstr) { //IP字符串转16进制
+//     char IP_String[] = {"255.255.255.255.."};
+//     memset(IP_String, 0, strlen("255.255.255.255.."));
+//     memcpy(IP_String, Ipstr, strlen(Ipstr));
+//     catString(IP_String, ".", strlen("255.255.255.255.."), 1); //字符串拼接
+//     char Str[4] = {0};
+//     char * P_piont = NULL;
+//     char * Head = IP_String;
+//     int temp = 0;
+//     for (int i = 0; i < 4; i++) {
+//         memset(Str, 0, 3);
+//         if ((P_piont = strchr(Head, '.')) != NULL) {
+//             *P_piont = '\0';
+//             memcpy(Str, Head, strlen(Head));
+//             *P_piont = '.';
+//             Head = P_piont + 1;
+//             temp = doneAsciiStrToAnyBaseNumberData(Str, 16);  //字符串转任意进制数
+//             IpHex.Name._char[i] = (unsigned char)temp;
+//         }
 //     }
-//     char TempBuff[ArraySize];
-//     memset(TempBuff, 0, ArraySize);
-//     if ((IsDouble) && (OutBuff.MaxLen >= ArraySize)) {
-//         memcpy(TempBuff, &Number, ArraySize);
-//         swapStr(TempBuff, ArraySize);
-//         memcpy(OutBuff.Name._char, TempBuff, ArraySize);
-//     } else if (OutBuff.MaxLen >= ArraySize) {
-//         float tempNum = (float)Number;
-//         memcpy(TempBuff, &tempNum, ArraySize);
-//         swapStr(TempBuff, ArraySize);
-//         memcpy(OutBuff.Name._char, TempBuff, ArraySize);
-//     }
-// #else
-//     /*Not C99*/
-//     int ArraySize = 0;
-//     if (IsDouble) {
-//         ArraySize = sizeof(double);
-//     } else {
-//         ArraySize = sizeof(float);
-//     }
-//     char * TempBuff = (char *)malloc(ArraySize);
-//     memset(TempBuff, 0, ArraySize);
-//     if ((IsDouble) && (OutBuff.MaxLen >= ArraySize)) {
-//         memcpy(TempBuff, &Number, ArraySize);
-//         swapStr(TempBuff, ArraySize);
-//         memcpy(OutBuff.Name._char, TempBuff, ArraySize);
-//     } else if (OutBuff.MaxLen >= ArraySize) {
-//         float tempNum = (float)Number;
-//         memcpy(TempBuff, &tempNum, ArraySize);
-//         swapStr(TempBuff, ArraySize);
-//         memcpy(OutBuff.Name._char, TempBuff, ArraySize);
-//     }
-// #endif
 // }
 
-// IPstrToHexArray 函数定义
-void IPstrToHexArray(strnew IpHex, const char * Ipstr) { //IP字符串转16进制
-    char IP_String[] = {"255.255.255.255.."};
-    memset(IP_String, 0, strlen("255.255.255.255.."));
-    memcpy(IP_String, Ipstr, strlen(Ipstr));
-    catString(IP_String, ".", strlen("255.255.255.255.."), 1); //字符串拼接
-    char Str[4] = {0};
-    char * P_piont = NULL;
-    char * Head = IP_String;
-    int temp = 0;
-    for (int i = 0; i < 4; i++) {
-        memset(Str, 0, 3);
-        if ((P_piont = strchr(Head, '.')) != NULL) {
-            *P_piont = '\0';
-            memcpy(Str, Head, strlen(Head));
-            *P_piont = '.';
-            Head = P_piont + 1;
-            temp = doneAsciiStrToAnyBaseNumberData(Str, 16);  //字符串转任意进制数
-            IpHex.Name._char[i] = (unsigned char)temp;
-        }
-    }
-}
+// // 主函数
+// //int main(int argc, char * argv[]) {
+// int main(void) {
+//     unsigned char NET_Remote_Url[] = "221.214.219.202\0";
+//     newString(EEprom_IP, 4);
+//     IPstrToHexArray(EEprom_IP, NET_Remote_Url);
+//     for (int i = 0; i < 4; i++) {
+//         printf("%2x %d\n", (EEprom_IP.Name._char[i] & 0xff), (unsigned char)(EEprom_IP.Name._char[i]));
+//     }
+//     return 0;
+// }
 
 // 主函数
-//int main(int argc, char * argv[]) {
-int main(void) {
-    unsigned char NET_Remote_Url[] = "221.214.219.202\0";
-    newString(EEprom_IP, 4);
-    IPstrToHexArray(EEprom_IP, NET_Remote_Url);
-    for (int i = 0; i < 4; i++) {
-        printf("%2x %d\n", (EEprom_IP.Name._char[i] & 0xff), (unsigned char)(EEprom_IP.Name._char[i]));
+int main(int argc, char * argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <-f / -d> <number>\n", argv[0]);
+        return 1;
     }
+    double number = doneAsciiToDouble(argv[2]);
+    newString(outBuff, sizeof(double));
+
+    int buffLen = 0;
+    if (strstr(argv[1], "-f") != NULL) {
+        DoubleOrFloatToBuff(outBuff, (float)number, false);
+        buffLen = 4;
+    } else if (strstr(argv[1], "-d") != NULL) {
+        DoubleOrFloatToBuff(outBuff, (double)number, true);
+        buffLen = 8;
+    } else {
+        fprintf(stderr, "Usage: %s <-f / -d> <number>\n", argv[0]);
+        return 1;
+    }
+
+    printf("%lf == ", number);
+    for (int i = 0; i < buffLen; i++) {
+        printf("%02x", (unsigned char)outBuff.Name._char[i] & 0xFF);
+    }
+    printf("\n");
+
     return 0;
 }
-
-
